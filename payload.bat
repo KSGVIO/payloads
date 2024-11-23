a08f1bc4e2b6a370d9296e5c8ac183463d3dea80
GVIO <119417271+KSGVIO@users.noreply.github.com>
2024-11-23 22:06:55 +0200

    Update payload.bat

--- payload.bat
+++ payload.bat
@@ -1,5 +1,5 @@
 @echo off
 for /l %%i in (1,1,5) do (
-    powershell -c (New-Object Media.SoundPlayer "%localappdata%\Programs\RemExec\payload\src\good.wav").PlaySync()
+    powershell -c (New-Object Media.SoundPlayer "%localappdata%\Programs\RemExec\payloads\src\good.wav").PlaySync()
 )
 exit
